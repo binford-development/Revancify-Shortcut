2a1fb50a3e43bc5b3510a67971f6c53496f2dedc
Michael Binford <162678184+binford-development@users.noreply.github.com>
2026-03-27 18:57:12 -0700

    Fix formatting mistake in install.sh

--- Revancify-Original/install.sh
+++ Revancify-Original/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 [ -z "$TERMUX_VERSION" ] && echo -e "Termux not detected !!" && exit 1
 BIN="$PREFIX/bin/revancify"
-curl -sL " https://github.com/binford-development/Revancify-Shortcut/raw/refs/heads/main/Revancify-Original/revancify" -o "$BIN"
+curl -sL "https://github.com/binford-development/Revancify-Shortcut/raw/refs/heads/main/Revancify-Original/revancify" -o "$BIN"
 [ -e "$BIN" ] && chmod +x "$BIN" && "$BIN"
